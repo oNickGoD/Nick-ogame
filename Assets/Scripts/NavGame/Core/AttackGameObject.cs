3f8f9288b0abb8298f251890b01007eacd929382
Nickao gostoso <firenze.age@gmail.com>
2020-03-28 12:42:37 -0300

    Saving enemies in aggro range video2 certo

--- Assets/Scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -4,42 +4,69 @@ using NavGame.Managers;
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace NavGame.Core {
+namespace NavGame.Core 
+{
     [RequireComponent (typeof (NavMeshAgent))]
 
-    public class AttackGameObject : TouchableGameObject {
+    public class AttackGameObject : TouchableGameObject 
+    {
         public OfenseStats ofenseStats;
+        public string[] enemyLayers;
+
+        [SerializeField]        
+        protected List<DamageableGameObject> enemiesToAttack = new List<DamageableGameObject>();
 
         protected NavMeshAgent agent;
         float cooldown = 0f;
+        LayerMask enemyMask;
 
         public OnAttackHitEvent onAttackHit;
 
         protected virtual void Awake()
         {
             agent = GetComponent<NavMeshAgent> ();
+            enemyMask = LayerMask.GetMask(enemyLayers);
         }
-        protected virtual void Update () {
+        protected virtual void Update () 
+        {
             DecreaseAttackCooldown ();
         }
 
-        public void AttackOnCooldown (DamageableGameObject target) {
-            if (cooldown <= 0f) {
+        public void AttackOnCooldown (DamageableGameObject target) 
+        {
+            if (cooldown <= 0f) 
+            {
                 cooldown = 1f / ofenseStats.attackSpeed;
                 target.TakeDamage (ofenseStats.damage);
-                if (onAttackHit != null) {
+                if (onAttackHit != null) 
+                {
                     onAttackHit (target.transform.position);
                 }
             }
         }
 
-        void DecreaseAttackCooldown () {
-            if (cooldown == 0f) {
+        void DecreaseAttackCooldown () 
+        {
+            if (cooldown == 0f) 
+            {
                 return;
             }
             cooldown -= Time.deltaTime;
-            if (cooldown < 0f) {
+            if (cooldown < 0f) 
+            {
                 cooldown = 0f;
+            }
+        }
+
+        void OnTriggerEnter(Collider other) 
+        {
+            if (enemyMask.Contains(other.gameObject.layer))
+            {
+                DamageableGameObject obj = other.transform.parent.GetComponent<DamageableGameObject>();
+                if (!enemiesToAttack.Contains(obj))
+                {
+                    enemiesToAttack.Add(obj);
+                }
             }
         }
     }
