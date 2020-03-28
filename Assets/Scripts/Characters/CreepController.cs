1d2a9a4e748a6fbf7a2c705dc3d58fb2dba0e8d5
Nickao gostoso <firenze.age@gmail.com>
2020-03-28 10:13:03 -0300

    Damageable and Attack Ind Classes

--- Assets/Scripts/Characters/CreepController.cs
+++ Assets/Scripts/Characters/CreepController.cs
@@ -1,51 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
 using NavGame.Core;
 using NavGame.Managers;
+using UnityEngine;
+using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
-public class CreepController : CombatGameObject
-{
-     NavMeshAgent agent;
-     DamageableGameObject finalTarget;
-  protected override void Awake()
-    {
-        base.Awake();
-        agent = GetComponent<NavMeshAgent>();
-        GameObject obj = GameObject.FindWithTag("Finish");
-        if (obj != null)
-        {
-            finalTarget = obj.GetComponent<DamageableGameObject>();
+[RequireComponent (typeof (NavMeshAgent))]
+public class CreepController : AttackGameObject {
+    NavMeshAgent agent;
+    DamageableGameObject finalTarget;
+    void Awake () {
+
+        agent = GetComponent<NavMeshAgent> ();
+        GameObject obj = GameObject.FindWithTag ("Finish");
+        if (obj != null) {
+            finalTarget = obj.GetComponent<DamageableGameObject> ();
         }
 
         onAttackHit += PlayEffects;
     }
-    protected override void Update()
-    {
-        base.Update();
-        if (finalTarget == null)
-        {
+    protected override void Update () {
+        base.Update ();
+        if (finalTarget == null) {
             return;
         }
-        if (IsInTouch(finalTarget))
-        {
-            AttackOnCooldown(finalTarget);
+        if (IsInTouch (finalTarget)) {
+            AttackOnCooldown (finalTarget);
         }
     }
 
-    void Start()
-    {
-        if (finalTarget != null)
-        {
-            agent.SetDestination(finalTarget.transform.position);
+    void Start () {
+        if (finalTarget != null) {
+            agent.SetDestination (finalTarget.transform.position);
         }
     }
 
-
-    void PlayEffects(Vector3 position)
-    {
-        AudioManager.instance.Play("enemy-hit", position);
+    void PlayEffects (Vector3 position) {
+        AudioManager.instance.Play ("enemy-hit", position);
     }
 }
