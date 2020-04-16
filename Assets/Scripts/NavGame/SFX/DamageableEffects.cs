ee119be47b404c19981d4c55247e6c7a9eb8c976
Nickao gostoso <firenze.age@gmail.com>
2020-04-16 11:18:08 -0300

    SFXController, Damageable effects

---
+++ Assets/Scripts/NavGame/SFX/DamageableEffects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+
+namespace NavGame.SFX
+{
+    [RequireComponent(typeof(DamageableGameObject))]
+    public class DamageableEffects : SFXController
+    {
+        public string damageSound;
+        public string dieSound;
+
+        public GameObject damageEffects;
+
+        public GameObject dieEffect;
+        void Awake()
+        {
+            DamageableGameObject damageable = GetComponent<DamageableGameObject>();
+            damageable.onDamageTaken += OnDamageTaken;
+            damageable.onDied += OnDied;
+        }
+
+        protected virtual void OnDamageTaken(Vector3 strikePoint, int amount)
+        {
+            PlayEffects(strikePoint, damageSound, damageEffects, Quaternion.identity);
+        }
+
+        protected virtual void OnDied()
+        {
+            PlayEffects(transform.position, dieSound, dieEffect, Quaternion.identity);
+        }
+
+    }
+}
