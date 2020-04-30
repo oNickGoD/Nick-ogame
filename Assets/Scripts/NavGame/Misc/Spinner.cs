bc97ca411c6961fb695e3af4ea63fc8c46a5a650
Nickao gostoso <firenze.age@gmail.com>
2020-04-30 11:20:59 -0300

    Projectile Spinner

---
+++ Assets/Scripts/NavGame/Misc/Spinner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace NavGame.Misc
+{
+    public class Spinner : MonoBehaviour
+    {
+        public Vector3 eulerPerSecond;
+        void Update()
+        {
+            transform.Rotate(eulerPerSecond * Time.deltaTime);
+        }
+    }
+}
