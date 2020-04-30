ff74adcbebd318273b7cf4fe09d0fd946148213e
Nickao gostoso <firenze.age@gmail.com>
2020-04-30 18:21:02 -0300

    TowerController

---
+++ Assets/Scripts/Characters/TowerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+
+public class TowerController : ThrowerGameObject
+{
+    protected virtual void UpdateAttack()
+    {
+        if (enemiesToAttack.Count > 0)
+        {
+            FaceObjectFrame(enemiesToAttack[0].gameObject.transform);
+            AttackOnCooldown(enemiesToAttack[0]);
+
+        }
+    }
+}
