ae91f5be6b1a09efde8eb143da37734f1d7ddcdb
Nickao gostoso <firenze.age@gmail.com>
2020-05-07 11:28:13 -0300

    Action move

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -10,10 +10,14 @@ public class PlayerController : TouchableGameObject
 
     Camera cam;
 
+    public float range = 4f;
+
     public LayerMask walkableLayer;
     public LayerMask collectibleLayer;
 
     CollectibleGameObject pickupTarget;
+
+    Vector3 actionPoint = Vector3.zero;
 
 
     void Awake()
@@ -29,6 +33,7 @@ public class PlayerController : TouchableGameObject
     {
         ProcessInput();
         UpdateCollect();
+        UpdateAction();
     }
 
     void ProcessInput()
@@ -54,6 +59,17 @@ public class PlayerController : TouchableGameObject
                 pickupTarget = null;
             }
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
+            {
+                actionPoint = hit.point;
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 
     void UpdateCollect()
@@ -63,6 +79,18 @@ public class PlayerController : TouchableGameObject
             if (IsInTouch(pickupTarget))
             {
                 pickupTarget.Pickup();
+            }
+        }
+    }
+
+    void UpdateAction()
+    {
+        if (actionPoint != Vector3.zero)
+        {
+            if (Vector3.Distance(transform.position, actionPoint) <= range)
+            {
+                agent.ResetPath();
+                actionPoint = Vector3.zero;
             }
         }
     }
