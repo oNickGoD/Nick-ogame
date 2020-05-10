2110312e37c03b9eebe42c8aac979f72b0e0fd1f
Nickao gostoso <firenze.age@gmail.com>
2020-05-10 19:37:20 -0300

    Wave update event

--- Assets/Scripts/NavGame/Core/Events.cs
+++ Assets/Scripts/NavGame/Core/Events.cs
@@ -18,4 +18,5 @@ namespace NavGame.Core
     public delegate void OnActionCooldownUpdateEvent(int actionIndex, float coolDown, float waitTime);
     public delegate void OnResourceUpdateEvent(int currentAmount);
     public delegate void OnReportableErrorEvent(string message);
+    public delegate void OnWaveUpdateEvent(int totalWaves, int currentWave);
 }
--- Assets/Scripts/NavGame/Managers/LevelManager.cs
+++ Assets/Scripts/NavGame/Managers/LevelManager.cs
@@ -18,6 +18,7 @@ namespace NavGame.Managers
         public OnActionCooldownUpdateEvent onActionCooldownUpdate;
         public OnResourceUpdateEvent onResourceUpdate;
         public OnReportableErrorEvent onReportableError;
+        public OnWaveUpdateEvent onWaveUpdate;
 
         protected int selectedAction = -1;
         protected LevelData levelData = new LevelData();
@@ -34,7 +35,7 @@ namespace NavGame.Managers
             }
         }
 
-        void Start()
+        protected virtual void Start()
         {
             StartCoroutine(SpawnBad());
         }
